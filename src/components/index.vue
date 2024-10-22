d1189fc6719b7a8f7eab8803bdd9dade984a3dd5
EvanYang <85303536+EvanYangtobegreatest@users.noreply.github.com>
2024-10-22 09:46:24 +0800

    Update index.vue

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="block text-center">
-    <el-carousel :interval="1500" arrow="never" height="100vh"  motion-blur
+    <el-carousel :interval="2000" arrow="never" height="100vh"  motion-blur
                  :autoplay="true" indicator-position="none" loop="true">
       <el-carousel-item v-for="(image, index) in images" :key="index">
         <img :src="image" alt="Carousel Image" class="carousel-image" />
@@ -42,7 +42,7 @@ export default {
       images: [
         'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
         'https://pub-c0e7f6c66b954c3a93e96fc7aa5425c6.r2.dev/%E6%94%B6%E8%97%8F/IMG_9079.JPG',
-        'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg'
+        'https://pub-c0e7f6c66b954c3a93e96fc7aa5425c6.r2.dev/%E6%94%B6%E8%97%8F/IMG_8148.JPG'
       ]
     };
   }
